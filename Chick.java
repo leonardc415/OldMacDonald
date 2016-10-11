b933aa0eaf091f16e28ebef4b1324d6404ae3961
leonardc415 <leonardc415@gmail.com>
2016-10-11 11:35:16 -0700

    farm

--- Chick.java
+++ Chick.java
@@ -1,4 +1,16 @@
 class Chick //implements Animal 
 {     
      //your code here
+	 private String myType;     
+     private String mySound;      
+     public Chick(String type, String sound)     {         
+         myType = type;         
+         mySound = sound;     
+     }     
+     public Chick()     {         
+         myType = "chicken";         
+         mySound = "cluck";     
+     }      
+     public String getSound(){return mySound;}     
+     public String getType(){return myType;} 
 }
--- Farm.java
+++ Farm.java
@@ -1,4 +1,16 @@
 class Farm 
 {     
    //your code here
+	private Animal[] aBunchofAnimals = new Animal[3];
+	public Farm()
+	{
+		aBunchofAnimals[0] = new Cow("cow", "Ben", "moo");
+		aBunchofAnimals[1] = new Chick("chicken", "cluck", "cheep");
+		aBunchofAnimals[2] = new Pig("pig", "oink");
+	}
+	public void animalSounds()
+	{
+		for(int nI=0; nI<aBunchofAnimals.length; nI++)
+			System.out.println(aBunchofAnimals[nI].getType() + "goes" + aBunchofAnimals[nI].getSound);
+	}
 }
