75f1872dd20ea9c5263f74d80067f1e3ac39a2c8
leonardc415 <leonardc415@gmail.com>
2016-10-18 11:27:56 -0700

    farm

--- Farm.java
+++ Farm.java
@@ -1,16 +1,18 @@
-class Farm 
+  class Farm  
 {     
-   //your code here
-	private Animal[] aBunchofAnimals = new Animal[3];
-	public Farm()
-	{
-		aBunchofAnimals[0] = new Cow("cow", "moo");
-		aBunchofAnimals[1] = new Chick("chicken", "cheep", "cluck");
-		aBunchofAnimals[2] = new Pig("pig", "oink");
-	}
-	public void animalSounds()
-	{
-		for(int nI=0; nI<aBunchofAnimals.length; nI++)
-			System.out.println(aBunchofAnimals[nI].getType() + "goes" + aBunchofAnimals[nI].getSound());
-	}
+    private Animal[] aBunchOfAnimals = new Animal[3];    
+    public Farm()  
+    {       
+       aBunchOfAnimals[0] = new NamedCow("cow","steve","moo");          
+       aBunchOfAnimals[1] = new Chick("chick","cheep","cluck");
+       aBunchOfAnimals[2] = new Pig("pig","oink");    
+    }     
+    public void animalSounds()    
+    {
+      for (int nI=0; nI < aBunchOfAnimals.length; nI++) 
+      	{             
+         	System.out.println( aBunchOfAnimals[nI].getType() + " goes " + aBunchOfAnimals[nI].getSound() );       
+      	}       
+         	System.out.println( "The cow is known as " + ((NamedCow)aBunchOfAnimals[0]).getName());    
+    } 	
 }
--- NamedCow.java
+++ NamedCow.java
@@ -1,4 +1,13 @@
 class NamedCow extends Cow
 {
-	
+	private String myName;
+	public Cow(String name)
+	{
+		myName = name;
+	}
+	public Cow()
+	{
+		myName = "steve";
+	}
+	void String getName(){return myName;}
 }
